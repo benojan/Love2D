4051b531d2cf7009f8594c67816c86f73d22deee
Benojan <skoj@qq.com>
2016-03-19 21:19:13 +0800

    Update love.graphics中文手册.lua

--- love.graphics中文手册.lua
+++ love.graphics中文手册.lua
@@ -1,5 +1,48 @@
+==配置函数==
+function love.conf(t)
+    t.identity = nil                    -- The name of the save directory (string)
+    t.version = "0.10.1"                -- 游戏使用的LÖVE引擎版本 (string)
+    t.console = false                   -- Attach a console (boolean, Windows only)
+    t.accelerometerjoystick = true      -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
+    t.externalstorage = false           -- True to save files (and read from the save directory) in external storage on Android (boolean) 
+    t.gammacorrect = false              -- Enable gamma-correct rendering, when supported by the system (boolean)
+ 
+    t.window.title = "Untitled"         -- The window title (string)
+    t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
+    t.window.width = 800                -- 窗口宽度 (number)
+    t.window.height = 600               -- 窗口高度 (number)
+    t.window.borderless = false         -- Remove all border visuals from the window (boolean)
+    t.window.resizable = false          -- 设置窗口是否可调整 (boolean)
+    t.window.minwidth = 1               -- 窗口最小宽度，若窗口可调整 (number)
+    t.window.minheight = 1              -- 窗口最小高度，若窗口可调整 (number)
+    t.window.fullscreen = false         -- 是否全屏 (boolean)
+    t.window.fullscreentype = "desktop" -- Choose between "desktop" fullscreen or "exclusive" fullscreen mode (string)
+    t.window.vsync = true               -- Enable vertical sync (boolean)
+    t.window.msaa = 0                   -- The number of samples to use with multi-sampled antialiasing (number)
+    t.window.display = 1                -- Index of the monitor to show the window in (number)
+    t.window.highdpi = false            -- Enable high-dpi mode for the window on a Retina display (boolean)
+    t.window.x = nil                    -- The x-coordinate of the window's position in the specified display (number)
+    t.window.y = nil                    -- The y-coordinate of the window's position in the specified display (number)
+ 
+    t.modules.audio = true              -- Enable the audio module (boolean)
+    t.modules.event = true              -- Enable the event module (boolean)
+    t.modules.graphics = true           -- Enable the graphics module (boolean)
+    t.modules.image = true              -- Enable the image module (boolean)
+    t.modules.joystick = true           -- Enable the joystick module (boolean)
+    t.modules.keyboard = true           -- Enable the keyboard module (boolean)
+    t.modules.math = true               -- Enable the math module (boolean)
+    t.modules.mouse = true              -- Enable the mouse module (boolean)
+    t.modules.physics = true            -- Enable the physics module (boolean)
+    t.modules.sound = true              -- Enable the sound module (boolean)
+    t.modules.system = true             -- Enable the system module (boolean)
+    t.modules.timer = true              -- Enable the timer module (boolean), Disabling it will result 0 delta time in love.update
+    t.modules.touch = true              -- Enable the touch module (boolean)
+    t.modules.video = true              -- Enable the video module (boolean)
+    t.modules.window = true             -- Enable the window module (boolean)
+    t.modules.thread = true             -- Enable the thread module (boolean)
+end
 
-                   love.graphics中文手册
+
 ==绘图方法==
 
 	弧(绘制模式:"fill"|"line",x座标,y座标,半径,起始弧度:x轴正方向为0,终止弧度:顺时针,线段数:越大则弧线越平滑)
